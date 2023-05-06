a41000a1ef2b040c9eeb2c8ce94194758769b603
umlx5h <umlx5h21@protonmail.com>
2023-05-06 09:23:44 +0000

    6日目-1

--- kilo.c
+++ kilo.c
@@ -4,8 +4,11 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
+
+#include <string.h>
 
 /*** defines ***/
 
@@ -13,7 +16,13 @@
 
 /*** data ***/
 
-struct termios orig_termios;
+struct editorConfig {
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
 
@@ -27,15 +36,15 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
 void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -55,7 +64,63 @@ char editorReadKey() {
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+    // カーソル位置の取得要求
+    // DSR – Device Status Report : ESC [ Ps n 	default value: 0
+    // 6 	Command from host – Please report active position (using a CPR control sequence)
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    // bufに取り出す
+    // src: ESC[30;120R, dst: ESC[30;120
+    while (i < sizeof(buf) - 1) {
+        // DSRをstdoutに投げるとstdinからカーソル位置を取得できる
+        // CPR – Cursor Position Report : ESC [ Pn ; Pn R 	default value: 1
+        //          30row 120column
+        // e.g. - ESC[30;120R
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    // バリデーション
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    // XX;YY の形式になっているのをパース
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        // ioctlでウィンドウサイズを取れなかった時にフォールバック版
+
+        // スクリーンサイズを取得するため、まずカーソルをスクリーン右下に移動する
+        // CUF – Cursor Forward  : ESC [ Pn C 	default value: 1    右に移動
+        // CUD – Cursor Down     : ESC [ Pn B 	default value: 1    下に移動
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+
+        // カーソル位置のrow, columnを取得
+        return getCursorPosition(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /*** output ***/
+
+void editorDrawRows() {
+    int y;
+    for (y = 0; y < E.screenrows; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
 
 void editorRefreshScreen() {
     // The \x1b is the ASCII escape character (hexadecimal value 0x1b = ESC) 
@@ -63,6 +128,10 @@ void editorRefreshScreen() {
     // コンソール全体をクリア : ED – Erase In Display
     write(STDOUT_FILENO, "\x1b[2J", 4);
     // カーソルを左上に移動 : CUP – Cursor Position
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
@@ -85,8 +154,13 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
