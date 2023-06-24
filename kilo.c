ed75070c6ff0cdc71b0fc3f8952295e484d043bd
umlx5h <umlx5h21@protonmail.com>
2023-06-24 18:22:19 +0900

    editor done

--- kilo.c
+++ kilo.c
@@ -94,6 +94,8 @@ struct editorConfig E;
 
 /*** prototypes ***/
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 /*** terminal ***/
 
@@ -434,8 +436,13 @@ void editorOpen(char *filename) {
 }
 
 void editorSave() {
-    if (E.filename == NULL)
-        return;
+    if (E.filename == NULL) {
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        if (E.filename == NULL) {
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
+    }
 
     // 1つの大きなバッファに文字列を作りファイルにそのまま書き込む
     int len;
@@ -652,6 +659,44 @@ void editorSetStatusMessage(const char *fmt, ...) {
 }
 
 /*** input ***/
+
+// ファイル保存用にステータスバーでファイル名を受け付ける
+char *editorPrompt(char *prompt) {
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while (1) {
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if (c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE) {
+            // 消せるようにする
+            if (buflen != 0) buf[--buflen] = '\0';
+        } else if (c == '\x1b') {
+            // ESCでキャンセル
+            editorSetStatusMessage("");
+            free(buf);
+            return NULL;
+        } else if (c == '\r') {
+            // ENTERで確定
+            if (buflen != 0) {
+                editorSetStatusMessage("");
+                return buf;
+            }
+        } else if (!iscntrl(c) && c < 128) { // if ASCII character (except control character)
+            // ASCII文字ならバッファに追加
+            if (buflen == bufsize - 1) { // バッファが足らなかったら再確保
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
+}
 
 void editorMoveCursor(int key) {
     // カーソル位置にある行を表す構造体を取得 (末尾の場合はNULL)
