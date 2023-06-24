a937daa635321100818d00bdb57db7adc779c7ae
umlx5h <umlx5h21@protonmail.com>
2023-06-24 12:52:07 +0900

    text viewer done

--- kilo.c
+++ kilo.c
@@ -7,11 +7,13 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 #include <signal.h>
@@ -58,7 +60,7 @@ void debug() {
 
 typedef struct erow {
     int size;    // 行の文字数 NULL文字も改行文字も入らない
-    int rsize;
+    int rsize;   // タブなど特殊文字を含めた文字数
     char *chars; // 行の文字列 NULL文字は入るが改行文字は入らない
     char *render;
 } erow;
@@ -77,6 +79,9 @@ struct editorConfig {
     // 構造体へのポインタ      malloc(sizeof(struct 構造体))   , struct->member
     // 構造体配列へのポインタ  malloc(sizeof(struct 構造体) * 要素数), struct[0].member
     erow *row;
+    char *filename;
+    char statusmsg[80];
+    time_t statusmsg_time;
     struct termios orig_termios;
 };
 
@@ -273,6 +278,9 @@ void editorAppendRow(char *s, size_t len) {
 /*** file i/o ***/
 
 void editorOpen(char *filename) {
+    // これ必要か？
+    free(E.filename);
+    E.filename = strdup(filename);
     FILE *fp = fopen(filename, "r");
     if (!fp) die("fopen");
 
@@ -394,12 +402,51 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "\x1b[K", 3);
 
         // 最後の行以外は改行を入れる
-        if (y < E.screenrows - 1) {
-            abAppend(ab, "\r\n", 2);
-        }
+        abAppend(ab, "\r\n", 2);
         // TODO: delete
         abFlush(ab, 1);
     }
+}
+
+void editorDrawStatusBar(struct abuf *ab) {
+    // 色を反転 (背景)
+    // SGR – Select Graphic Rendition
+    // 7 	Negative (reverse) image
+    abAppend(ab, "\x1b[7m", 4);
+    char status[80], rstatus[80];
+    // ファイル名と行数を描画
+    int len = snprintf(status, sizeof(status), "%.20s - %d lines",
+        E.filename ? E.filename : "[No Name]", E.numrows);
+    
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
+        E.cy + 1, E.numrows);
+    if (len > E.screencols)
+        len = E.screenrows;
+    abAppend(ab, status, len);
+
+    // 残りのスペースを埋める
+    while (len < E.screencols) {
+        // 右端に位置していた場合に編集行の情報を描画
+        if (E.screencols - len == rlen) {
+            abAppend(ab, rstatus, rlen);
+            break;
+        } else {
+            abAppend(ab, " ", 1);
+            len++;
+        }
+    }
+    // 戻す
+    abAppend(ab, "\x1b[m", 3);
+    abAppend(ab, "\r\n", 2);
+}
+
+void editorDrawMessageBar(struct abuf *ab) {
+    abAppend(ab, "\x1b[K", 3);
+    int msglen = strlen(E.statusmsg);
+    if (msglen > E.screencols)
+        msglen = E.screencols;
+    if (msglen && time(NULL) - E.statusmsg_time < 5)
+        abAppend(ab, E.statusmsg, msglen);
 }
 
 // @see: https://vt100.net/docs/vt100-ug/chapter3.html
@@ -421,6 +468,8 @@ void editorRefreshScreen() {
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
+    editorDrawStatusBar(&ab);
+    editorDrawMessageBar(&ab);
 
     // カーソル位置を現在の位置に移動
     char buf[32];
@@ -437,6 +486,14 @@ void editorRefreshScreen() {
     abFree(&ab);
 }
 
+void editorSetStatusMessage(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+    va_end(ap);
+
+    E.statusmsg_time = time(NULL);
+}
 
 /*** input ***/
 
@@ -537,8 +594,12 @@ void initEditor() {
     E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
+    E.filename = NULL;
+    E.statusmsg[0] = '\0';
+    E.statusmsg_time = 0;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+    E.screenrows -= 2;
 }
 
 void debugScreen() {
@@ -555,6 +616,8 @@ int main(int argc, char *argv[]) {
     if (argc >= 2) {
         editorOpen(argv[1]);
     }
+
+    editorSetStatusMessage("HELP: Ctrl-q = quit");
 
     while (1) {
         // スクリーンに文字を描画
